bdfc31c84ef5429a9063f6fe22717b5db998f6aa
siddharth ninawe <siddharth.ninawe.0@gmail.com>
2021-10-14 13:47:31 +0530

    cmake adding

---
+++ build/CMakeFiles/mycpp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mycpp.dir/main.cpp.obj"
+  "CMakeFiles/mycpp.dir/main.cpp.obj.d"
+  "libmycpp.dll.a"
+  "mycpp.exe"
+  "mycpp.exe.manifest"
+  "mycpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mycpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
