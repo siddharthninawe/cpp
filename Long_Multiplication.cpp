42e6c39a5cab2ac95f2f1b6ec2536e68fe2ee4cd
siddharth ninawe <siddharth.ninawe.0@gmail.com>
2021-10-23 18:25:12 +0530

    cutting paper square challenge

---
+++ Long_Multiplication.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    long long value1 = 689715240;
+    long long value2 = 759842301;
+    long long ans;
+    ans = value1 * value2;
+    cout << ans-1;
+}
