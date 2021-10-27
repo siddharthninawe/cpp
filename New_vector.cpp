48f35a9a8a1b9548a166ae49b4dd3718ccd33407
siddharth ninawe <siddharth.ninawe.0@gmail.com>
2021-10-27 20:37:54 +0530

    array manipulation code optimized

---
+++ New_vector.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    vector<char> vec(5);
+    for(int i = 0; i < vec.size(); i++) {
+        cout << vec.at(i) << endl;
+    }
+    return 0;
+}
